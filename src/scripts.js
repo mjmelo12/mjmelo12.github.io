bdc0e2377639a1cf3901e7a279bdcf5babe29740
Kevin Zhang <96510861+kzhang20123123@users.noreply.github.com>
2022-04-25 16:06:40 -0700

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,12 @@
 // Add scripts here
+// For our menu bar
+
+var menu = $('nav#menu');
+
+var watcher = scrollMonitor.create( menu );
+
+watcher.lock();
+
+watcher.stateChange(function() {
+  $(menu).toggleClass('scrolled', this.isAboveViewport)
+});
