fcd56a9cf2eeb4aa481984a8acde6ca0886cd377
mjmelo12 <91216492+mjmelo12@users.noreply.github.com>
2022-05-05 00:16:03 -0700

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -18,8 +18,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 34.0967, lng: -117.179 },
+    zoom: 14,
   });
 }
 
